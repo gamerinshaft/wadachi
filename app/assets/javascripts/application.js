50a6143409344d723c0906f0e7f31d83c2469f00
j.taneichi <j.tane3ma@gmail.com>
2015-08-19 22:54:20 -0700

    add controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require react
 //= require react_ujs
 //= require components
-//= require_tree .
 //= require websocket_rails/main
+//= require_tree .
+
 
---
+++ app/controllers/websocket_profile_controller.rb
@@ -0,0 +1,19 @@
+# WebsocketRails::BaseControllerを継承
+
+class WebsocketProfileController < WebsocketRails::BaseController
+
+  def message_recieve
+
+　　# クライアントからのメッセージを取得
+
+　　recieve_message = message()
+
+
+
+　　# websocket_chatイベントで接続しているクライアントにブロードキャスト
+
+　　broadcast_message(:websocket_profile, recieve_message)
+
+  end
+
+end

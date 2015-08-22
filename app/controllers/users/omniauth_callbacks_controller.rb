06bdd7fdb291fbc4d43ed7da12762528cc35154a
j.taneichi <j.tane3ma@gmail.com>
2015-08-22 16:37:37 -0700

    bug fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    Rails.logger.debug("-----------------\n#{request.env["omniauth.auth"]}\n------------------")
-    @user = User.find_or_create_by(user_params)
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
-    else
+    Rails.logger.debug("-----------------\n#{uid}\n------------------")
+    @user = User.find_or_initialize_by(uid)
+    if @user.new_record?
+      @user.update_attributes(user_params)
       @user.profile = Profile.new()
+      @user.flag = Flag.new()
+      @user.notification = Notification.new()
+      @user.save
       session["devise.github_data"] = request.env["omniauth.auth"]
       sign_in_and_redirect @user, :event => :authentication
+    else
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     end
   end
 
   private
+  def uid
+    params = request.env["omniauth.auth"].slice(:uid).to_h
+  end
   def user_params
     params = request.env["omniauth.auth"].slice(:provider, :uid).to_h
     params["github_token"] = request.env["omniauth.auth"][:credentials][:token]

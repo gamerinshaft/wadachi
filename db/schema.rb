8c52c08f1a1e262a074982646f8b95e2044216bc
j.taneichi <j.tane3ma@gmail.com>
2015-08-21 19:01:27 -0700

    rails g model Profile bio:text user:references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819105605) do
+ActiveRecord::Schema.define(version: 20150822020059) do
+
+  create_table "profiles", force: true do |t|
+    t.text     "bio"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

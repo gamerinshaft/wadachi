93f2bddced0e6ee1429508f34982e0cf245b7b58
j.taneichi <j.tane3ma@gmail.com>
2015-08-23 01:55:46 -0700

    rails g controller Api::User index new create update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
   namespace :api do
+  get 'user/index'
+  end
+
+  namespace :api do
+  get 'user/new'
+  end
+
+  namespace :api do
+  get 'user/create'
+  end
+
+  namespace :api do
+  get 'user/update'
+  end
+
+  namespace :api do
+  get 'user/destroy'
+  end
+
+  namespace :api do
   get 'dashboards/index'
   end
 

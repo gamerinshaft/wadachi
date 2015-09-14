fdbbe72256e977cf13a4d4923345b170359f55e2
j.taneichi <j.tane3ma@gmail.com>
2015-09-14 15:36:33 +0900

    upd

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,10 @@ class DashboardController < ApplicationController
     @client = Octokit::Client.new access_token: current_user.github_token
     @hoge = @client
     @repos = @client.repositories(current_user.nickname)
+    @repos.each do |hoge|
+      Rails.logger.debug(@client.languages(hoge.full_name).inspect)
+      # Rails.logger.debug(@client.languages(hoge))
+    end
     @github = @client.user
     @user = current_user
     @flag = @user.flag

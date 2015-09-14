439eaf9d3f49a3c479c75c7366e0cab5286407d5
j.taneichi <j.tane3ma@gmail.com>
2015-09-14 15:44:27 +0900

    setting has_one

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   devise :omniauthable, :trackable
   has_one :profile, dependent: :destroy
   has_one :notification, dependent: :destroy
+  has_one :github, dependent: :destroy
   has_one :flag, dependent: :destroy
 
   def filtered_hash

ee41937bff2005cbc4c1507ef888f4d003976b7a
j.taneichi <j.tane3ma@gmail.com>
2015-09-29 19:30:00 +0900

    upd

--- db/schema.rb
+++ db/schema.rb
@@ -60,24 +60,24 @@ ActiveRecord::Schema.define(version: 20150915005724) do
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.string   "provider",               limit: 255
-    t.string   "uid",                    limit: 255
-    t.string   "github_token",           limit: 255
-    t.string   "avatar_url",             limit: 255
-    t.string   "nickname",               limit: 255
-    t.string   "name",                   limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "github_token"
+    t.string   "avatar_url"
+    t.string   "nickname"
+    t.string   "name"
   end
 
   add_index "users", ["uid"], name: "index_users_on_uid", unique: true

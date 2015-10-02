6368221b1ff7fba72c6702ed3ad19fd4e59f8913
j.taneichi <j.tane3ma@gmail.com>
2015-10-02 23:31:57 +0900

    debug test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
     @client = Octokit::Client.new access_token: current_user.github_token
     @user = current_user
     @repos = @client.repositories(current_user.nickname)
+    @islands = Island.all
     @flag = @user.flag
     filter = %i[name email uid avatar_url]
     @users = User.select(filter).page

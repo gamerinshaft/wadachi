80b278a7e330a5bb87bb76f2c15bcf38bd342ca4
j.taneichi <j.tane3ma@gmail.com>
2015-10-09 02:32:07 +0900

    add percentage judge

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -18,16 +18,19 @@ class Achievement < ActiveRecord::Base
         name = "#{lang.name}コミッター初級"
         params = {name: name, content: "#{lang.name}について100行以上記述する"}
         params[:status] = lang.count >=100 ?  1 : 0
+        params[:percentage] = lang.count >=100 ? 100 : ((lang.count/100.to_f)*100).to_i
         create_or_update(user, name, params)
 
         name = "#{lang.name}コミッター中級"
         params = {name: name, content: "#{lang.name}について1000行以上記述する"}
         params[:status] = lang.count >=1000 ?  1 : 0
+        params[:percentage] = lang.count >=1000 ? 100 : ((lang.count/1000.to_f)*100).to_i
         create_or_update(user, name, params)
 
         name = "#{lang.name}コミッター上級"
         params = {name: name, content: "#{lang.name}について10000行以上記述する"}
         params[:status] = lang.count >=10000 ?  1 : 0
+        params[:percentage] = lang.count >=10000 ? 100 : ((lang.count/10000.to_f)*100).to_i
         create_or_update(user, name, params)
       end
     end
@@ -36,16 +39,19 @@ class Achievement < ActiveRecord::Base
       name = "プロジェクトメイカー初級"
       params = {name: name, content: "リポジトリを一つ作る"}
       params[:status] = user.github.repositories.count >=1 ?  1 : 0
+      params[:percentage] = user.github.repositories.count >=1 ? 100 : ((user.github.repositories.count/1.to_f)*100).to_i
       create_or_update(user, name, params)
 
       name = "プロジェクトメイカー中級"
       params = {name: name, content: "リポジトリを五つ作る"}
       params[:status] = user.github.repositories.count >=5 ?  1 : 0
+      params[:percentage] = user.github.repositories.count >=5 ? 100 : ((user.github.repositories.count/5.to_f)*100).to_i
       create_or_update(user, name, params)
 
       name = "プロジェクトメイカー上級"
       params = {name: name, content: "リポジトリを十個作る"}
       params[:status] = user.github.repositories.count >=10 ?  1 : 0
+      params[:percentage] = user.github.repositories.count >=10 ? 100 : ((user.github.repositories.count/10.to_f)*100).to_i
       create_or_update(user, name, params)
     end
 

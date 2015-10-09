d544141a6f84e206a85fae0bd5ead9b36b3abf32
j.taneichi <j.tane3ma@gmail.com>
2015-10-09 11:39:01 +0900

    edit dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,11 @@ class DashboardController < ApplicationController
     @client = Octokit::Client.new access_token: current_user.github_token
     @user = current_user
     @repos = @client.repositories(current_user.nickname)
-    @islands = Island.all
+    @pass_islands = []
+    current_user.pass_areas.each do |area|
+      @pass_islands.push area.island
+    end
+    @pass_islands.uniq!
     @flag = @user.flag
     filter = %i[name email uid avatar_url]
     @users = User.select(filter).page

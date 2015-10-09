04e75eacb11cbefaa0656748cf2e23c8fc748191
j.taneichi <j.tane3ma@gmail.com>
2015-10-10 03:35:30 +0900

    edit dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,38 @@
 class DashboardController < ApplicationController
   def index
-    @client = Octokit::Client.new access_token: current_user.github_token
+    # @client = Octokit::Client.new access_token: current_user.github_token
     @user = current_user
-    @repos = @client.repositories(current_user.nickname)
-    @pass_islands = []
+
+    hash = Hash.new{|h,k| h[k]= []}
+    @pass_islands = Hash.new{|h,k| h[k]= []}
     current_user.pass_areas.each do |area|
-      @pass_islands.push area.island
+      hash[:islands].push area.island
     end
-    @pass_islands.uniq!
+    @data = {}
+    hash[:islands].uniq!
+    hash[:islands].each do |island|
+      areas = current_user.pass_areas.where(island_id: island.id)
+      island = JSON.parse(island.to_json).compact
+      island[:areas] = JSON.parse(areas.to_json).compact
+      @pass_islands[:islands].push island
+    end
+
+    @languages = {}
+    User.all.each do |user|
+      user.github.languages.each do |lang|
+        @languages[lang.name.to_sym] ||= 0
+        @languages[lang.name.to_sym] += lang.count
+      end
+    end
+    @languages = @languages.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
+    @languages = @languages.to_a
+
     @flag = @user.flag
     filter = %i[name email uid avatar_url]
     @users = User.select(filter).page
   end
+
 end
+
+
+

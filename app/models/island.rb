190ce3d5e2fae82807617a2559b889c7d9f261ff
j.taneichi <j.tane3ma@gmail.com>
2015-10-10 03:34:45 +0900

    taskの移し替え

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,3 +1,27 @@
 class Island < ActiveRecord::Base
   has_many :areas, dependent: :destroy
+  class << self
+    def update
+      languages = {}
+      User.all.each do |user|
+        user.github.languages.each do |language|
+          if  languages.key?(language.name.to_sym)
+            languages[language.name.to_sym] += language.count
+          else
+            languages[language.name.to_sym] = language.count
+          end
+        end
+      end
+      languages.each do |lang, count|
+        if Island.exists?(name: lang)
+          Island.update(power: count)
+        else
+          island = Island.create(name: lang, power: count)
+          island.areas.create(name: "初めての#{lang}")
+          island.areas.create(name: "#{lang}の村")
+          island.areas.create(name: "#{lang}上級特区")
+        end
+      end
+    end
+  end
 end
